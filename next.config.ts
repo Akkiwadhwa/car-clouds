6defa6dc7ec456ff369116c0558947c2b8873ce4
Akkiwadhwa <akkiwadhwa1995@gmail.com>
2024-11-20 00:33:57 +0530

    Added 'www.carlogos.org' to the list of domains in the images config.

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ["images.squarespace-cdn.com"], // Add the external domain here
+    domains: ["images.squarespace-cdn.com",'www.carlogos.org'], // Add the external domain here
   },
 };
 
